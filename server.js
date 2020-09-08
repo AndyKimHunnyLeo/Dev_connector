e9428b84683100f4c23624c85fe10dbbd491800a
Andy Kim <hyungSeok.kim@outlook.com>
2020-09-08 14:37:28 +1200

    minor bbug fix

--- server.js
+++ server.js
@@ -3,19 +3,17 @@ const connectDB = require('./config/db');
 const path = require('path');
 const app = express();
 
-//connect Database
+// Connect Database
 connectDB();
 
-//Init Middleware
-app.use(express.json({ extended: false }));
+// Init Middleware
+app.use(express.json());
 
-// app.get('/', (req, res) => res.send('API Running'));
-
-//Define Routes
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
